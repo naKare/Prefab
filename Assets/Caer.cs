cb9a9a6ce66b7afcd135a8d3b2f124773d6b78e7
Karen Nava <na466657@uaeh.edu.mx>
2024-04-30 22:48:47 -0600

    Actividad completada

---
+++ Assets/Caer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Cubo;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Instantiate(Cubo, transform.position, Quaternion.identity);
+
+        }
+    }
+}
